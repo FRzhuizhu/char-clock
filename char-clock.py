761e792c85b9527f1dff97119bec48bcf38d069b
FRzhuizhu <736019332@qq.com>
2019-07-20 15:02:16 +0800

    create and commit test

---
+++ char-clock.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+import time
+
+
+class chr_clock(object):
+
+    def __init__(self):
+        pass
+
+
+    def get_time(self):
+        pass
+
+
+    def display_by_polar_coordinates(self):
+        pass
+
+
